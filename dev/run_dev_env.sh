3fb239594c0806acf504cc4ba17667961142d4af
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-07 13:32:17 +0000

    Changed .gitlab-ci.yml

--- dev/run_dev_env.sh
+++ dev/run_dev_env.sh
@@ -6,4 +6,5 @@ docker run -it --rm \
   -v /Users/markchassy/git/kudos/env:/prj/env \
   -v /Users/markchassy/git/kudos/config:/prj/config \
   -v /Users/markchassy/git/kudos/dev:/prj/dev \
+  -v /Users/markchassy/git/kudos/build:/prj/build \
   kudos-env:latest
