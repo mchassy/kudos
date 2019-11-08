280342e42b0bb8131b4f006fc230e98496aa15eb
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-08 18:31:08 +0000

    Changed .gitlab-ci.yml

--- dev/build_dev_env.sh
+++ dev/build_dev_env.sh
@@ -1,7 +1,2 @@
-#docker rmi $DOCKER_URI/kudos-env:latest
-#docker rmi kudos-env:latest
-docker build -t kudos-env:latest .
-if [ $? -eq 0 ]
-then
-  docker tag kudos-env:latest $DOCKER_URI/kudos-env:latest && docker push $DOCKER_URI/kudos-env:latest
-fi
+docker build -t $DOCKER_URI/kudos-env:latest .
+docker push $DOCKER_URI/kudos-env:latest
--- dev/run_dev_env.sh
+++ dev/run_dev_env.sh
@@ -7,4 +7,4 @@ docker run -it --rm \
   -v /Users/markchassy/git/kudos/config:/prj/config \
   -v /Users/markchassy/git/kudos/dev:/prj/dev \
   -v /Users/markchassy/git/kudos/build:/prj/build \
-  kudos-env:latest
+  $DOCKER_URI/kudos-env:latest
