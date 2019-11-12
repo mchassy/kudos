270e439b5002113b46befa9b7ebc7dc469b57b26
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-12 11:44:12 +0000

    Changed .gitlab-ci.yml

--- config/development.py
+++ config/development.py
@@ -4,4 +4,4 @@ MONGO_SERVER = "localhost"
 MONGO_URL = mongodb://$MONGO_ADMINUSERNAME/$MONGODB_ADMINPASSWORD@$MONGO_SERVER:27017/
 APP_SERVER = "localhost"
 APP_PORT = "8080"
-ENV_NAME = "default"
+ENV_NAME = "development"
