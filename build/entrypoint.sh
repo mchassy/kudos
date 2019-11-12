3a1338ce9659aba4bc5a9927ca22a9aac44cf281
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-12 15:23:04 +0000

    Changed .gitlab-ci.yml

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export FLASK_APP=/prj/kudos/kudos_oss/app/http/api/endpoints.py
-export APP_CONFIG_FILE=/prj/kudos/config/development.py
+export APP_CONFIG_FILE=/prj/kudos/config/integration.py
 export FLASK_ENV={{ execution_environment }}
 python3 -m flask run --port 4433 &
 echo "FLASK_APP is $FLASK_APP"
