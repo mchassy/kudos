1b710d1930960152daf61f854f3e26d0590ce424
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-13 16:31:17 +0000

    Changed indents

--- app/http/api/middlewares.py
+++ app/http/api/middlewares.py
@@ -9,14 +9,14 @@ def login_required(f):
         authorization = request.headers.get("authorization", None)
         if not authorization:
             return json.dumps({'error': 'no authorization token provied'}), 403,
-                {'Content-type': 'application/json'}
+        {'Content-type': 'application/json'}
         try:
             token = authorization.split(' ')[1]
             resp = decode(token, None, verify=False, algorithms=['HS256'])
             g.user = resp['sub']
         except exceptions.DecodeError as identifier:
             return json.dumps({'error': 'invalid authorization token'}), 403,
-                {'Content-type': 'application/json'}
+         {'Content-type': 'application/json'}
 
         return f(*args, **kwargs)
 
