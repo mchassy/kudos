d6a189b776dfcfbcb8fdf7b025bd6e28605f7f4f
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-13 16:26:46 +0000

    Changed indents

--- app/http/api/endpoints.py
+++ app/http/api/endpoints.py
@@ -16,13 +16,11 @@ def index():
 @app.route("/kudos", methods=["POST"])
 @login_required
 def create():
-   github_repo = GithubRepoSchema().load(json.loads(request.data))
-
-   if github_repo.errors:
-       return json_response({'error': github_repo.errors}, 422)
-
-   kudo = Kudo(g.user).create_kudo_for(github_repo)
-   return json_response(kudo)
+    github_repo = GithubRepoSchema().load(json.loads(request.data))
+    if github_repo.errors:
+        return json_response({'error': github_repo.errors}, 422)
+    kudo = Kudo(g.user).create_kudo_for(github_repo)
+    return json_response(kudo)
 
 @app.route("/kudo/<int:repo_id>", methods=["GET"])
 @login_required
@@ -37,16 +35,13 @@ def show(repo_id):
 @login_required
 def update(repo_id):
     github_repo = GithubRepoSchema().load(json.loads(request.data))
-
     if github_repo.errors:
         return json_response({'error': github_repo.errors}, 422)
-
     kudo_service = Kudo(g.user)
     if kudo_service.update_kudo_with(repo_id, github_repo):
         return json_response(github_repo.data)
     else:
         return json_response({'error': 'kudo not found'}, 404)
-
 
 @app.route("/kudo/<int:repo_id>", methods=["DELETE"])
 @login_required
--- app/http/api/middlewares.py
+++ app/http/api/middlewares.py
@@ -10,15 +10,14 @@ def login_required(f):
         if not authorization:
             return json.dumps({'error': 'no authorization token provied'}), 403,
                 {'Content-type': 'application/json'}
+        try:
+            token = authorization.split(' ')[1]
+            resp = decode(token, None, verify=False, algorithms=['HS256'])
+            g.user = resp['sub']
+        except exceptions.DecodeError as identifier:
+            return json.dumps({'error': 'invalid authorization token'}), 403,
+                {'Content-type': 'application/json'}
 
-            try:
-                token = authorization.split(' ')[1]
-                resp = decode(token, None, verify=False, algorithms=['HS256'])
-                g.user = resp['sub']
-            except exceptions.DecodeError as identifier:
-                return json.dumps({'error': 'invalid authorization token'}), 403,
-                    {'Content-type': 'application/json'}
+        return f(*args, **kwargs)
 
-                return f(*args, **kwargs)
-
-                return wrap
+    return wrap
