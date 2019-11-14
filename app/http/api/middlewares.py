74ddbe887db4baabf5cee52485a612281a5dff48
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-14 10:40:08 +0000

    Changed .gitlab-ci.yml

--- app/http/api/middlewares.py
+++ app/http/api/middlewares.py
@@ -9,15 +9,15 @@ def login_required(f):
     def wrap(*args, **kwargs):
         authorization = request.headers.get("authorization", None)
         if not authorization:
-            return json.dumps({'error': 'no authorization token provied'})
-            , 403, {'Content-type': 'application/json'}
+            return json.dumps({'error': 'no authorization token provied'}), 403,
+            {'Content-type': 'application/json'}
         try:
             token = authorization.split(' ')[1]
             resp = decode(token, None, verify=False, algorithms=['HS256'])
             g.user = resp['sub']
         except exceptions.DecodeError as identifier:
-            return json.dumps({'error': 'invalid authorization token'})
-            , 403, {'Content-type': 'application/json'}
+            return json.dumps({'error': 'invalid authorization token'}), 403,
+            {'Content-type': 'application/json'}
 
         return f(*args, **kwargs)
 
