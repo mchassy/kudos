6e40c6b5fe70a0fdd4d00621aa15b56c4433d4e0
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-14 16:00:57 +0000

    Changed .gitlab-ci.yml

--- dev/entrypoint.sh
+++ dev/entrypoint.sh
@@ -6,33 +6,41 @@ export APP_CONFIG_FILE=$PRJ_DIR/config/development.py
 export FLASK_ENV=development
 # export LC_ALL=C.UTF-8
 # export LANG=C.UTF-8
-if [[ $CI_JOB_STAGE == "" ]]
+echo "STARTING ENTRYPOINT"
+echo "PWD is $PWD"
+echo "LISTING FOLDERS"
+ls -al
+if [[ -z "${CI_JOB_STAGE}" ]]
 then
     CI_JOB_STAGE="locally"
-elif [[ $CI_JOB_STAGE == "build_image" ]]
+fi
+echo "CI_JOB_STAGE is $CI_JOB_STAGE"
+echo "Running $CI_JOB_STAGE"
+if [[ $CI_JOB_STAGE == "build_image" ]]
 then
     echo "Buildah Login"
     aws ecr get-login --no-include-email --region us-east-1 >&1 | $(sed 's/docker/buildah/')
 fi
-echo "Running $CI_JOB_STAGE"
 if test -f "./env/bin/activate"; then
+    echo "#######################################################"
+    echo "Activate virtual environment"
+    echo "#######################################################"
     source ./env/bin/activate
 else
     echo "#######################################################"
-    echo "Creating virtual environment"
+    echo "Creating and activate virtual environment"
     echo "#######################################################"
     python3 -m venv env
     source ./env/bin/activate
 fi
-echo "#######################################################"
-echo "Virtual environment activated"
-echo "#######################################################"
 # if ! test -f "./env/bin/flask"; then
 if [[ $CI_JOB_STAGE -eq "locally" ]] && ! test -f "./env/bin/flask"
 then
     echo "#######################################################"
     echo "Installing pip requirments"
     echo "#######################################################"
+    ls -al ./env/bin
     pip install -r ./dev/requirements.txt
 fi
+
 bash
