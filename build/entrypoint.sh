896b04a1b7f68bc60d414cbcd429e5c082dafb04
Mark Chassy <mark.chassy@virtualclarity.com>
2019-11-22 16:19:52 +0000

    Changed .gitlab-ci.yml

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -6,6 +6,7 @@ export APP_CONFIG_FILE=/config/{{ execution_environment }}.py
 export FLASK_ENV={{ execution_environment }}
 # python3 -m flask run --port 4433 &
 cd /app/http/web/app
-npm start
-
+npm start &
+sleep 5
+echo "Go to http://localhost:3001"
 bash
--- dev/kstart.sh
+++ dev/kstart.sh
@@ -1,4 +1,4 @@
-docker run --rm -it \
+docker run --rm -t -d \
 --name kudos_test \
 -p 3001:3000 \
 508384386034.dkr.ecr.us-east-1.amazonaws.com/kudos:latest
